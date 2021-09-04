6bd7b736a5c4353f79e3285cc54dd0f661d8dc20
mehdizr <mahdi_zarei78@yahoo.com>
2021-09-04 20:27:18 +0430

    fix some styles use onSubmitted , keyboardType

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -8,6 +8,16 @@ class NewTransaction extends StatelessWidget {
   NewTransaction(this.addTxHandller);
   @override
   Widget build(BuildContext context) {
+    void submitData() {
+      final enteredTitle = titleController.text;
+      final enteredAmount = double.parse(amountController.text);
+      if (enteredTitle.isEmpty || enteredAmount <= 0) {
+        return;
+      } 
+        addTxHandller(enteredTitle, enteredAmount);
+      
+    }
+
     return Card(
       child: Container(
         padding: EdgeInsets.all(10),
@@ -15,25 +25,22 @@ class NewTransaction extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             TextField(
-              decoration: InputDecoration(labelText: 'Amount'),
-              controller: titleController,
-            ),
-            TextField(
+                onSubmitted: (_) => submitData(),
                 decoration: InputDecoration(
                   labelText: 'Title',
                 ),
-                controller: amountController),
-            
-              
-              FlatButton(
-                textColor: Colors.purple,
-                onPressed: () {
-                  addTxHandller(
-                      titleController.text, double.parse(amountController.value.text));
-                },
-                child: Text('Add Amount'),
-              ),
-            
+                controller: titleController),
+            TextField(
+              onSubmitted: (_) => submitData(),
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+              keyboardType: TextInputType.number,
+            ),
+            FlatButton(
+              textColor: Colors.purple,
+              onPressed: () => submitData(),
+              child: Text('Add Amount'),
+            ),
           ],
         ),
       ),
