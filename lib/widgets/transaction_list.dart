84cb54aea2b410e006c906cd302754db17b15034
mehdizr <mahdi_zarei78@yahoo.com>
2021-09-04 18:22:16 +0430

    add ListView.Builder() instead SingleChildView hig

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,6 +9,19 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 700,
+      /**** 
+      
+
+      //ListView.Builder give  us high performance more than ListView for ulimits List when we use it we
+      // dont need to use .map( ) for build our list . 
+      //ListView.Builder has 2 important attributes 
+      //1 . itemCount : we have to say to this class we want to render how many items
+      //2 . itemBuilder: we have to define the anonymous function with 2 arguments(first: its type of BuilderContext, second: its type of int) line: 27
+      //second argument give correct number of my transactions array to function
+      //and first argument complate by Flutter and make our list
+
+
+    ****/
       child: ListView.builder(
           itemCount: transactions.length,
           itemBuilder: (BuildContext context, int index) {
