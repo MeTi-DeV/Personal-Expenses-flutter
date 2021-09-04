e58e9fdd6fda3689d4030b31d877cc83f8b0efbe
mehdizr <mahdi_zarei78@yahoo.com>
2021-09-04 18:08:39 +0430

    add ListView.Builder() instead SingleChildView

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -8,10 +8,10 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height:700,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tx) {
+      height: 700,
+      child: ListView.builder(
+          itemCount: transactions.length,
+          itemBuilder: (BuildContext context, int index) {
             return (Card(
               child: Row(
                 children: [
@@ -20,9 +20,7 @@ class TransactionList extends StatelessWidget {
                     decoration: BoxDecoration(
                         border: Border.all(color: Colors.purple, width: 2)),
                     margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-      
-      
-                    child: Text('${tx.amount} \$',
+                    child: Text('${transactions[index].amount} \$',
                         style: TextStyle(
                             color: Colors.purple,
                             fontSize: 20,
@@ -31,25 +29,22 @@ class TransactionList extends StatelessWidget {
                   Column(
                     children: [
                       Container(
-                        
                         padding: EdgeInsets.only(bottom: 12),
-                        child: Text(tx.title.toUpperCase(),
+                        child: Text(transactions[index].title.toUpperCase(),
                             style: TextStyle(
                                 fontSize: 16, fontWeight: FontWeight.bold)),
                       ),
-      
-      
                       Container(
-                          child: Text(DateFormat('yyyy-MM-dd | hh : mm : ss').format(tx.date),
+                          child: Text(
+                              DateFormat('yyyy-MM-dd | hh : mm : ss')
+                                  .format(transactions[index].date),
                               style: TextStyle(color: Colors.grey)))
                     ],
                   )
                 ],
               ),
             ));
-          }).toList(),
-        ),
-      ),
+          }),
     );
   }
 }
